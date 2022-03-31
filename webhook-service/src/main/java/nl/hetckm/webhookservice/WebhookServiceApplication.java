6e17162d8bc404dc7ab0748831ec5b5234b85457
Tijs Groenendaal <tijs.groenendaal@gmail.com>
2022-03-31 13:59:15 +0200

    🚧 create module for webhooksService

---
+++ webhook-service/src/main/java/nl/hetckm/webhookservice/WebhookServiceApplication.java
@@ -0,0 +1,18 @@
+package nl.hetckm.webhookservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+
+
+@SpringBootApplication(scanBasePackages = "nl.hetckm")
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EntityScan(basePackages = "nl.hetckm.base.model.webhook")
+public class WebhookServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebhookServiceApplication.class, args);
+    }
+
+}
---
+++ webhook-service/src/test/java/nl/hetckm/webhookservice/WebhookServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.hetckm.webhookservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebhookServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
